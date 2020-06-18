f8883f337d97e72beb19938dbebfe639f303398b
pratikpjain <63181338+pratikpjain@users.noreply.github.com>
2020-06-18 14:27:49 +0530

    added files

---
+++ index.js
@@ -0,0 +1,27 @@
+var name1 = prompt('Enter the name of Player 1 ');
+var name2 = prompt('Enter the name of Player 2 ');
+var first = document.getElementById('p1');
+first.innerHTML = name1;
+var sec = document.getElementById('p2');
+sec.innerHTML = name2;
+function die() {
+    var sc1 = Math.floor(Math.random() * 6) + 1;
+    var sc2 = Math.floor(Math.random() * 6) + 1;
+    var p1 = document.getElementById('player1');
+    var p2 = document.getElementById('player2');
+    p1.innerText = sc1;
+    p2.innerText = sc2;
+    if(sc1 > sc2) {
+        var y = document.getElementById('result')
+        y.innerHTML = '<img class = "flag" src = "winner.jpg"><h1>Congrats , ' + name1 + ' Wins!!!</h1>';
+    }
+    else if (sc1 < sc2) {
+        var y = document.getElementById('result')
+        y.innerHTML = '<img class = "flag" src = "winner.jpg"><h1>Congrats , ' + name2 + ' Wins!!!</h1>';
+    }
+    else {
+        var y = document.getElementById('result')
+        y.innerHTML = '<h1>Opps ,Draw!!!</h1>';
+    } 
+
+}
